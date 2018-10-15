7146cbc8ee5b82f9f3ad00b372a84dadb5e875ce
Alex Mayer <amayer5125@gmail.com>
2018-10-15 11:56:47 -0400

    Use if err, return Style
    Add newlines before return to make code more readable

--- main.go
+++ main.go
@@ -16,50 +16,42 @@ func parseArgs() []string {
 	// Check for at least one arg, bail if none
 	if len(os.Args) < 2 {
 		log.Fatalln("You must provide at least one GitHub username.  Usernames can be provided either as a comma-separated list or as separate arguments.")
-	} else if len(os.Args) == 2 {
-		//  One argument, comma-separated list
-		if strings.Contains(os.Args[1], ",") {
-			// We have a comma, so assume multiple users
-			usernames := strings.Split(os.Args[1], ",")
-			return usernames
-		} else {
-			// No comma, one username provided
-			usernames := os.Args[1:]
-			return usernames
-		}
-	} else {
-		// Here we assume usernames were provided as separate args, maybe with commas
-		usernames := os.Args[1:]
-		j := 0
-		for i := 0; i < len(usernames); i++ {
-			usernames[j] = strings.Replace(usernames[i], ",", "", -1)
-			j++
-		}
-		usernames = usernames[:j]
-		return usernames
 	}
-	return make([]string, 0)
+
+	// We have one arg possible comma separated
+	if len(os.Args) == 2 {
+		return strings.Split(os.Args[1], ",")
+	}
+
+	// Here we assume usernames were provided as separate args, maybe with commas
+	usernames := os.Args[1:]
+	j := 0
+	for i := 0; i < len(usernames); i++ {
+		usernames[j] = strings.Replace(usernames[i], ",", "", -1)
+		j++
+	}
+	usernames = usernames[:j]
+
+	return usernames
 }
 
 func checkUsername(username string) bool {
-	valid := false
-
 	log.Printf("Checking for GitHub user %s", username)
+
 	url := fmt.Sprintf("https://github.com/%s.keys", username)
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if response.StatusCode == 200 {
-		log.Printf("Found valid user %s", username)
-		valid = true
-		return valid
-	} else {
+	if response.StatusCode != 200 {
 		log.Printf("%s is an invalid user", username)
+		return false
 	}
 
-	return valid
+	log.Printf("Found valid user %s", username)
+
+	return true
 }
 
 func addUser(username string) {
@@ -81,6 +73,7 @@ func addUser(username string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return
 }
 
@@ -104,6 +97,7 @@ func fetchKeys(username string) {
 	if err != nil {
 		log.Println(err)
 	}
+
 	return
 }
 
@@ -111,6 +105,7 @@ func addSshDir(username string) {
 	log.Printf("Adding .ssh directory for user %s", username)
 	path := fmt.Sprintf("/home/%s/.ssh", username)
 	os.Mkdir(path, 0700)
+
 	return
 }
 
@@ -125,18 +120,23 @@ func fixPerms(username string) {
 	os.Chmod(keyfile, 0600)
 	os.Chown(sshdir, uid, gid)
 	os.Chown(keyfile, uid, gid)
+
 	return
 }
 
 func main() {
 	usernames := parseArgs()
+
 	for _, username := range usernames {
-		if checkUsername(username) {
-			addUser(username)
-			addSshDir(username)
-			fetchKeys(username)
-			fixPerms(username)
+		if !checkUsername(username) {
+			continue
 		}
+
+		addUser(username)
+		addSshDir(username)
+		fetchKeys(username)
+		fixPerms(username)
 	}
+
 	return
 }
